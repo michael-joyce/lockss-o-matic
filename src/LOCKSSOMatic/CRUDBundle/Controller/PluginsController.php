00863134ff445e18dffd10962c911f0cec441120
Michael Joyce <ubermichael@gmail.com>
2015-01-21 16:07:10 -0800

    Start moving the plnplugin code to a service, and use that service in a file upload form to import a jar file.

--- src/LOCKSSOMatic/CRUDBundle/Controller/PluginsController.php
+++ src/LOCKSSOMatic/CRUDBundle/Controller/PluginsController.php
@@ -53,63 +53,6 @@ class PluginsController extends Controller
             'entities' => $entities,
         ));
     }
-    /**
-     * Creates a new Plugins entity.
-     *
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Plugins();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('plugins_show', array('id' => $entity->getId())));
-        }
-
-        return $this->render('LOCKSSOMaticCRUDBundle:Plugins:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
-     * Creates a form to create a Plugins entity.
-     *
-     * @param Plugins $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Plugins $entity)
-    {
-        $form = $this->createForm(new PluginsType(), $entity, array(
-            'action' => $this->generateUrl('plugins_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
-
-    /**
-     * Displays a form to create a new Plugins entity.
-     *
-     */
-    public function newAction()
-    {
-        $entity = new Plugins();
-        $form   = $this->createCreateForm($entity);
-
-        return $this->render('LOCKSSOMaticCRUDBundle:Plugins:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
 
     /**
      * Finds and displays a Plugins entity.
@@ -132,79 +75,7 @@ class PluginsController extends Controller
             'delete_form' => $deleteForm->createView(),
         ));
     }
-
-    /**
-     * Displays a form to edit an existing Plugins entity.
-     *
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('LOCKSSOMaticCRUDBundle:Plugins')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Plugins entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('LOCKSSOMaticCRUDBundle:Plugins:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-    * Creates a form to edit a Plugins entity.
-    *
-    * @param Plugins $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Plugins $entity)
-    {
-        $form = $this->createForm(new PluginsType(), $entity, array(
-            'action' => $this->generateUrl('plugins_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
-     * Edits an existing Plugins entity.
-     *
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('LOCKSSOMaticCRUDBundle:Plugins')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Plugins entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('plugins_edit', array('id' => $id)));
-        }
-
-        return $this->render('LOCKSSOMaticCRUDBundle:Plugins:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+    
     /**
      * Deletes a Plugins entity.
      *
--- src/LOCKSSOMatic/PLNImporterBundle/Controller/DefaultController.php
+++ src/LOCKSSOMatic/PLNImporterBundle/Controller/DefaultController.php
@@ -2,16 +2,59 @@
 
 namespace LOCKSSOMatic\PLNImporterBundle\Controller;
 
+use LOCKSSOMatic\PLNImporterBundle\Services\PLNPluginImportService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 
 #use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+
     public function indexAction()
     {
-        $response = $this->render('LOCKSSOMaticPLNImporterBundle:Default:index.html.twig', array());
+        $response = $this->render('LOCKSSOMaticPLNImporterBundle:Default:index.html.twig',
+            array());
         $response->headers->set('Content-Type', 'text/html');
         return $response;
     }
+
+    private function createPluginUploadForm()
+    {
+        return $this->createFormBuilder()
+                ->add('jar_file', 'file',
+                    array(
+                    'label'    => 'Upload .jar file',
+                    'required' => true,
+                ))
+                ->add('save', 'submit',
+                    array(
+                    'label'        => 'Import',
+                    'button_class' => 'btn btn-primary'
+                ))
+                ->getForm();
+    }
+
+    public function importPluginAction(Request $request)
+    {
+        $form = $this->createPluginUploadForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            /** @var UploadedFile */
+            $file = $form['jar_file']->getData();
+            /** @var PLNPluginImportService */
+            $importer = $this->container->get('pln_plugin_importer');
+            $entity = $importer->importJarFile($file, false);
+            $this->getDoctrine()->getEntityManager()->flush();
+            return $this->redirect($this->generateUrl('plugins_show', array(
+                'id' => $entity->getId())
+            ));
+        }
+        return $this->render('LOCKSSOMaticPLNImporterBundle:Default:plugin.html.twig',
+                array(
+                'upload_form' => $form->createView()
+        ));
+    }
+
 }
