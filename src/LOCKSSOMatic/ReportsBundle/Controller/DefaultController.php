15bc3e0bc56c3ee4e65f4be34b3339dcfc48d314
Marcus Emmanuel Barnes <mbarnes@sfu.ca>
2015-01-27 17:26:07 -0800

    Initial commit of ReportsBundle skeleton.

---
+++ src/LOCKSSOMatic/ReportsBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LOCKSSOMatic\ReportsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LOCKSSOMaticReportsBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/LOCKSSOMatic/ReportsBundle/LOCKSSOMaticReportsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LOCKSSOMatic\ReportsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LOCKSSOMaticReportsBundle extends Bundle
+{
+}
